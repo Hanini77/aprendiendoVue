37512a1ea4ac0951dcc26ee94cd05bd7409c7554
Hanny Carballo <hannycarballo@UDCC3-ESCANER.local>
2025-07-01 18:36:14 -0600

    moviendo a la carpeta hola-mundo 2 archivos

--- index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-// Borra el contenido HTML existente
-document.body.innerHTML = '<div id="app"></div>';
-
-// Renderiza tu componente React en su lugar
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>¡Hola, mundo!</h1>);
