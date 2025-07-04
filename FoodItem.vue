631d3766bfef85599c829b2333a8dcaf6afc6e45
Hanny Carballo <hannycarballo@UDCC3-ESCANER.local>
2025-07-03 18:03:52 -0600

    ejemplos para la documentación

---
+++ FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Manzana',
+      message: 'Me gustan las manzanaaas 🍎'
+    }
+  }
+};
+</script>
+
+<style></style>
